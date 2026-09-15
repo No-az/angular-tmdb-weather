b650b05c780f800e022caf5a1abcb31673b40b5b
Noaz <juancucunuba5@gmail.com>
2026-09-15 18:19:50 -0500

    Auto-generated commit

---
+++ chunk-3KDZKOAL.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-DDNMOJVL.js").then(o=>o.MoviesPageComponent)}];export{t as MOVIES_ROUTES};
---
+++ chunk-A6SCEYVX.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-3KHDH7JI.js").then(o=>o.WeatherPageComponent)}];export{t as WEATHER_ROUTES};
